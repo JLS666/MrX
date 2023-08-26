b4e27583e315591e1fb9828e5180f238ce28925a
JLS666 <43670180+JLS666@users.noreply.github.com>
2023-08-26 20:28:35 +0200

    Großes Aufräumen

--- js/main.js
+++ js/main.js
@@ -15,8 +15,52 @@
 // initialize Leaflet
 var map = L.map('map').setView({lon: 9.5, lat: 49.2}, 12);
 
+// HTML Elemente
 const ausgabe = document.getElementById("debugInfo");
 
+// Markers
+var markerMrX = L.AwesomeMarkers.icon({   // Marker Mister X
+  icon: 'user-secret',
+  prefix: 'fa',
+  markerColor: 'black',
+  extraClasses: ''
+});
+var markerRotate = L.AwesomeMarkers.icon({   // Marker Drehen
+  icon: 'rotate',
+  prefix: 'fa',
+  markerColor: 'orange',
+  extraClasses: ''
+});
+var markerGhost = L.AwesomeMarkers.icon({   // Marker Geist
+  icon: 'ghost',
+  prefix: 'fa',
+  markerColor: 'orange',
+  extraClasses: ''
+});
+var markerBullhorn = L.AwesomeMarkers.icon({   // Marker Megaphon
+  icon: 'bullhorn',
+  prefix: 'fa',
+  markerColor: 'orange',
+  extraClasses: ''
+});
+var markerCamera = L.AwesomeMarkers.icon({   // Marker Kamera
+  icon: 'camera',
+  prefix: 'fa',
+  markerColor: 'orange',
+  extraClasses: ''
+});
+var markerSplotch = L.AwesomeMarkers.icon({   // Marker Fleck
+  icon: 'splotch',
+  prefix: 'fa',
+  markerColor: 'orange',
+  extraClasses: ''
+});
+var markerClock = L.AwesomeMarkers.icon({   // Marker Zeit
+  icon: 'clock-rotate-left',
+  prefix: 'fa',
+  markerColor: 'orange',
+  extraClasses: 'fa-flip'
+});
 /******************* Funktionen ********************************/
 function startUpLeaflet() {
   // add the OpenStreetMap tiles
@@ -35,70 +79,42 @@ function randomPlacement() {
   // Inspiration: https://www.daftlogic.com/sandbox-leaflet-maps-plot-random.htm
 }
 
-function playAudio() {
-  new Audio("https://samplelib.com/lib/preview/mp3/sample-15s.mp3").play()
+function drawCircle(latitude, longitude, radius) {
+  var circle = L.circle([latitude, longitude], {
+      color: 'red',
+      fillColor: '#f03',
+      fillOpacity: 0.1,
+      radius: radius
+  }).addTo(map);
 }
+
+function playAudio(link) {  // Link muss ein String sein
+  new Audio(link).play()
+  // Beispielaufruf: playAudio("https://samplelib.com/lib/preview/mp3/sample-15s.mp3");
+}
+
 /******************* Hauptlogik ********************************/
 startUpLeaflet();
-//new Audio("Ridin.mp3").play()
-playAudio();
+drawCircle(49.2, 9.5, 300);
 
-// Creates a red marker with the coffee icon
-var redMarker = L.AwesomeMarkers.icon({
-  icon: 'coffee',
-  prefix: 'fa',
-  markerColor: 'red'
-});
-L.marker([49.2,8.5], {icon: redMarker}).addTo(map);
-
-var blueMarker = L.AwesomeMarkers.icon({
-  icon: 'j',
-  prefix: 'fa',
-  markerColor: 'blue'
-});
-L.marker([49.25,8.5], {icon: blueMarker}).addTo(map);
-
-L.marker([49.27, 8.5], {icon: L.AwesomeMarkers.icon({icon: 'ambulance', prefix: 'fa', markerColor: 'darkgreen'}) }).addTo(map);
-
+// Mr. X
+L.marker([49.29,8.52], {icon: markerMrX}).bindPopup("Julian").addTo(map);
 // Karte umdrehen
-L.marker([49.28, 8.5], {icon: L.AwesomeMarkers.icon({icon: 'rotate', prefix: 'fa', markerColor: '#333254'}) }).addTo(map);
-
+L.marker([49.28, 8.5], {icon: markerRotate}).addTo(map);
 // Tarnkappe
-L.marker([49.29, 8.5], {icon: L.AwesomeMarkers.icon({icon: 'ghost', prefix: 'fa', markerColor: 'green'}) }).addTo(map);
-
+L.marker([49.29, 8.5], {icon: markerGhost}).addTo(map);
 // Schrei
-L.marker([49.30, 8.5], {icon: L.AwesomeMarkers.icon({icon: 'bullhorn', prefix: 'fa', markerColor: 'darkgreen'}) }).addTo(map);
-
+L.marker([49.30, 8.5], {icon: markerBullhorn}).addTo(map);
 // Kamera
-L.marker([49.31, 8.5], {icon: L.AwesomeMarkers.icon({icon: 'camera', prefix: 'fa', markerColor: 'blue'}) }).addTo(map);
-
+L.marker([49.28, 8.51], {icon: markerCamera}).addTo(map);
 // Fleck auf Karte erzeugen
-L.marker([49.32, 8.5], {icon: L.AwesomeMarkers.icon({icon: 'splotch', prefix: 'fa', markerColor: 'purple'}) }).addTo(map);
-
+L.marker([49.29, 8.51], {icon: markerSplotch}).addTo(map);
 // Zeit
-L.marker([49.33, 8.5], {icon: L.AwesomeMarkers.icon({icon: 'clock-rotate-left', prefix: 'fa', markerColor: 'darkpurple', extraClasses: 'fa-flip'}) }).bindPopup("Power-up: Zeit").addTo(map);
-
+L.marker([49.30, 8.51], {icon: markerClock}).bindPopup("Power-up: Zeit").addTo(map);
 // A
-L.marker([49.34, 8.5], {icon: L.AwesomeMarkers.icon({icon: 'a', prefix: 'fa', markerColor: 'cadetblue'}) }).addTo(map);
+L.marker([49.28, 8.52], {icon: L.AwesomeMarkers.icon({icon: 'a', prefix: 'fa', markerColor: 'cadetblue', extraClasses: 'fa-flip'}) }).bindPopup("Aziza").addTo(map);
 
-
-
-L.marker([51.941196,4.512291], {icon: L.AwesomeMarkers.icon({icon: 'spinner', prefix: 'fa', markerColor: 'red', spin:true}) }).addTo(map);
-L.marker([51.927913,4.521303], {icon: L.AwesomeMarkers.icon({icon: 'coffee', prefix: 'fa', markerColor: 'red', iconColor: '#f28f82'}) }).addTo(map);
-L.marker([51.936063,4.502077], {icon: L.AwesomeMarkers.icon({icon: 'cog', prefix: 'fa', markerColor: 'purple', iconColor: 'black', extraClasses: 'fa-flip'}) }).addTo(map);
-L.marker([51.932835,4.506969], {icon: L.AwesomeMarkers.icon({icon: 'glass', prefix: 'fa', markerColor: 'green', extraClasses: 'fa-beat'}) }).addTo(map);
-L.marker([51.930295,4.515209], {icon: L.AwesomeMarkers.icon({icon: 'shopping-cart', prefix: 'fa', markerColor: 'blue', extraClasses: 'fa-bounce'}) }).addTo(map);
-L.marker([51.930083,4.507742], {icon: L.AwesomeMarkers.icon({icon: 'info', prefix: 'fa', markerColor: 'orange'}) }).addTo(map);
-
-L.marker([51.930454,4.527054], {icon: L.AwesomeMarkers.icon({icon: 'group', prefix: 'fa', markerColor: 'darkred'}) }).addTo(map);
-L.marker([51.929607,4.527054], {icon: L.AwesomeMarkers.icon({icon: 'arrow-right', prefix: 'fa', markerColor: 'darkblue'}) }).addTo(map);
-L.marker([51.928919,4.528856], {icon: L.AwesomeMarkers.icon({icon: 'twitter', prefix: 'fa', markerColor: 'cadetblue'}) }).addTo(map);
-L.marker([51.930295,4.530745], {icon: L.AwesomeMarkers.icon({icon: 'phone', prefix: 'fa', markerColor: 'darkpurple'}) }).addTo(map);
-L.marker([51.925055,4.512806], {icon: L.AwesomeMarkers.icon({icon: 'ambulance', prefix: 'fa', markerColor: 'darkgreen'}) }).addTo(map);
-L.marker([51.925902,4.505768], {icon: L.AwesomeMarkers.icon({icon: 'medkit', prefix: 'fa', markerColor: 'darkblue'}) }).addTo(map);
-
-
-
+farbenTest();
 /******************* Ereignisbehandlung ************************/
 
 
@@ -106,21 +122,71 @@ L.marker([51.925902,4.505768], {icon: L.AwesomeMarkers.icon({icon: 'medkit', pre
 
 
 
+
+
+
+/******************* Spielwiese *******************************/
+
+
+function farbenTest()
+{
+  // Farbentest
+  L.marker([49.28, 8.53], {icon: L.AwesomeMarkers.icon({icon: 'b', prefix: 'fa', markerColor: 'red'}) }).addTo(map);
+  L.marker([49.29, 8.53], {icon: L.AwesomeMarkers.icon({icon: 'c', prefix: 'fa', markerColor: 'darkred'}) }).addTo(map);
+  L.marker([49.30, 8.53], {icon: L.AwesomeMarkers.icon({icon: 'd', prefix: 'fa', markerColor: 'lightred'}) }).addTo(map);
+  L.marker([49.28, 8.54], {icon: L.AwesomeMarkers.icon({icon: 'e', prefix: 'fa', markerColor: 'orange'}) }).addTo(map);
+  L.marker([49.29, 8.54], {icon: L.AwesomeMarkers.icon({icon: 'f', prefix: 'fa', markerColor: 'beige'}) }).addTo(map);
+  L.marker([49.30, 8.54], {icon: L.AwesomeMarkers.icon({icon: 'g', prefix: 'fa', markerColor: 'green'}) }).addTo(map);
+  L.marker([49.28, 8.55], {icon: L.AwesomeMarkers.icon({icon: 'h', prefix: 'fa', markerColor: 'darkgreen'}) }).addTo(map);
+  L.marker([49.29, 8.55], {icon: L.AwesomeMarkers.icon({icon: 'i', prefix: 'fa', markerColor: 'lightgreen'}) }).addTo(map);
+  L.marker([49.30, 8.55], {icon: L.AwesomeMarkers.icon({icon: 'j', prefix: 'fa', markerColor: 'blue'}) }).addTo(map);
+  L.marker([49.28, 8.56], {icon: L.AwesomeMarkers.icon({icon: 'k', prefix: 'fa', markerColor: 'darkblue'}) }).addTo(map);
+  L.marker([49.29, 8.56], {icon: L.AwesomeMarkers.icon({icon: 'l', prefix: 'fa', markerColor: 'lightblue'}) }).addTo(map);
+  L.marker([49.30, 8.56], {icon: L.AwesomeMarkers.icon({icon: 'm', prefix: 'fa', markerColor: 'purple'}) }).addTo(map);
+  L.marker([49.28, 8.57], {icon: L.AwesomeMarkers.icon({icon: 'n', prefix: 'fa', markerColor: 'darkpurple'}) }).addTo(map);
+  L.marker([49.29, 8.57], {icon: L.AwesomeMarkers.icon({icon: 'o', prefix: 'fa', markerColor: 'pink'}) }).addTo(map);
+  L.marker([49.30, 8.57], {icon: L.AwesomeMarkers.icon({icon: 'p', prefix: 'fa', markerColor: 'cadetblue'}) }).addTo(map);
+  L.marker([49.28, 8.58], {icon: L.AwesomeMarkers.icon({icon: 'q', prefix: 'fa', markerColor: 'white', iconColor: 'darkgrey'}) }).addTo(map);
+  L.marker([49.29, 8.58], {icon: L.AwesomeMarkers.icon({icon: 'r', prefix: 'fa', markerColor: 'gray'}) }).addTo(map);
+  L.marker([49.30, 8.58], {icon: L.AwesomeMarkers.icon({icon: 's', prefix: 'fa', markerColor: 'lightgray'}) }).addTo(map)
+  L.marker([49.30, 8.59], {icon: L.AwesomeMarkers.icon({icon: 't', prefix: 'fa', markerColor: 'black'}) }).addTo(map);
+}
+
+
 // show a marker on the map
-L.marker({lon: -0.11, lat: 51.508}, {color: 'red'}).bindPopup("<b>Hello world!</b><br>I am a popup.").addTo(map);
-
-var circle = L.circle([49.2, 9.5], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.1,
-    radius: 300
-}).addTo(map);
+var info = L.marker({lon: -0.11, lat: 51.508}, {opacity: 0.8}).bindPopup("<b>Hello world!</b><br>I am a popup.").addTo(map);
+console.log(info);
 
 
 
+// Funktion, die bei Änderungen der Position aufgerufen wird
+function positionChanged(position) {
+  var latitude = position.coords.latitude;
+  var longitude = position.coords.longitude;
+
+  console.log("Neue Position - Breitengrad: " + latitude + ", Längengrad: " + longitude);
+  positionCallback(position);
+}
+
+// Funktion, die bei Fehlern aufgerufen wird
+function positionError(error) {
+  console.error("Fehler bei der Positionsabfrage:", error.message);
+}
+
+// Überprüfen, ob der Browser Geolocation unterstützt
+if ("geolocation" in navigator) {
+  // Position überwachen
+  var watchId = navigator.geolocation.watchPosition(positionChanged, positionError);
+
+  // Um die Überwachung zu beenden:
+  // navigator.geolocation.clearWatch(watchId);
+} else {
+  console.log("Geolocation wird nicht unterstützt.");
+}
 
 
 
+/*
 
 
 const intervalID2 = setInterval(getLocation, 5000);
@@ -175,10 +241,11 @@ options = {
   maximumAge: 10000
 };
 
-
+*/
 
 function positionCallback(position) {
   //wann die Position ermittelt wurde
+  /*
     console.log("Zeitpunkt: " + position.timestamp);
 
     console.log("Latitude: " + position.coords.latitude); //dezimal Grad
@@ -191,13 +258,6 @@ function positionCallback(position) {
 
     console.log("Geschw: " + position.coords.speed); //Meter pro Sek.
     console.log("Richtung: " + position.coords.heading); //Grad von wahrem Norden
+    */
     ausgabe.innerHTML = "Zeitpunkt: " + position.timestamp + ", Latitude: " + position.coords.latitude + ", Longitude: " + position.coords.longitude + ", Genauigkeit in m: " + position.coords.accuracy + ", Höhe: " + position.coords.altitude + ", Genauigkeit in m: " + position.coords.altitudeAccuracy + ", Geschw: " + position.coords.speed + ", Richtung: " + position.coords.heading;
 }
-
-
-
-
-
-
-
-
